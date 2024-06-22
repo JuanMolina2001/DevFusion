811f3e1faba2e41d03f98703126b2333517cdc23
JuanMolina2001 <juanmolinajm2001@gmail.com>
2024-06-22 04:26:25 -0400

    Remove installer.html and installer.py files

--- msc/installer.py
+++
@@ -1,9 +0,0 @@
-import webview
-
-
-window = webview.create_window('DevFusion', 'installer.html', width=800, height=480, resizable=False)
-class api:
-    def select_folder(self):
-        return window.create_file_dialog(webview.FOLDER_DIALOG)
-window._js_api = api()
-webview.start(debug=True)
