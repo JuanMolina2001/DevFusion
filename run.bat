140ff2ab1c60e107a7f6cba5f7a992929a08c9b8
JuanMolina2001 <juanmolinajm2001@gmail.com>
2024-06-22 04:51:34 -0400

    Update run.bat script to include npm command support

--- run.bat
+++ run.bat
@@ -12,6 +12,12 @@ IF "%1" EQU "start" (
     .env\Scripts\activate.bat
 ) ELSE IF "%1" EQU "deactivate" (
     .env\Scripts\deactivate.bat
+)  ELSE IF "%1" EQU "npm" (
+    IF "%2" NEQ "" (
+        npm --prefix client %2 %3 %4 %5 %6 %7 %8 %9
+    ) ELSE (
+        echo No se ha especificado un comando para npm.
+    )
 ) ELSE (
     echo Comando no reconocido. Por favor, usa uno de los siguientes:
     echo start - Para ejecutar app.py
